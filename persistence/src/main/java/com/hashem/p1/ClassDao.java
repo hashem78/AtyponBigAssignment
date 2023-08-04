1a4be03e036995f446b2ed3b6b90a3aec0da033c
hashem78 <hashem.olayano@gmail.com>
2023-08-04 23:51:26 +0300

    Empty classes should be included

--- persistence/src/main/java/com/hashem/p1/ClassDao.java
+++ persistence/src/main/java/com/hashem/p1/ClassDao.java
@@ -26,14 +26,13 @@ public class ClassDao implements AutoCloseable {
                     Classes.name AS class_name,
                     Users.id AS user_id,
                     Users.email,
-                    Users.password,
                     Roles.name AS role_name,
                     Roles.id as role_id
                 FROM
                     Classes
-                        INNER JOIN
+                        LEFT JOIN
                     UserClasses ON Classes.id = UserClasses.class_id
-                        INNER JOIN
+                        LEFT JOIN
                     Users ON UserClasses.user_id = Users.id
                         LEFT JOIN
                     UserRoles ON Users.id = UserRoles.user_id
@@ -50,13 +49,17 @@ public class ClassDao implements AutoCloseable {
             var clazz = classesMap.getOrDefault(classId, new CClass(classId, className, new HashSet<>()));
 
             var userId = resultSet.getInt("user_id");
-            var userEmail = resultSet.getString("email");
-            var userPassword = resultSet.getString("password");
-            var user = usersMap.getOrDefault(userId, new User(userId, userEmail, userPassword, new HashSet<>()));
-            user.roles().add(new Role(resultSet.getInt("role_id"), resultSet.getString("role_name")));
-            usersMap.put(userId, user);
 
-            clazz.users().add(user);
+            if (userId != 0) {
+                var userEmail = resultSet.getString("email");
+
+                var user = usersMap.getOrDefault(userId, new User(userId, userEmail, "", new HashSet<>()));
+                user.roles().add(new Role(resultSet.getInt("role_id"), resultSet.getString("role_name")));
+                usersMap.put(userId, user);
+
+                clazz.users().add(user);
+            }
+
             classesMap.put(classId, clazz);
         }
         return new HashSet<>(classesMap.values());
